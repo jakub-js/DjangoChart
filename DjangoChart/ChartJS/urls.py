c70f6ef60ec860971b3f4e4fc5dac0b015c83e62
jakub-js <szczepanski.jakub06@gmail.com>
2022-10-27 13:28:12 +0200

    init5

--- DjangoChart/ChartJS/urls.py
+++ DjangoChart/ChartJS/urls.py
@@ -1,11 +1,16 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import ChartViewSet
+from .views import ChartViewSet, home, index, get_data
 
 router = routers.DefaultRouter()
 router.register(r'ChartJS', ChartViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('api-auth/', include(r'rest_framework.urls', namespace='rest__framework'))
+    path('api-auth/', include(r'rest_framework.urls', namespace='rest__framework')),
+    path('', home, name='home'),
+    path('index', index, name='index'),
+    path('data', get_data, name='data')
+
+
 ]
--- DjangoChart/ChartJS/views.py
+++ DjangoChart/ChartJS/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+import pyodbc
+import pandas as pd
+import json
+
 from .models import Chart
 from rest_framework import viewsets
 from .serializer import ChartSerializer
@@ -8,3 +13,32 @@ from .serializer import ChartSerializer
 class ChartViewSet(viewsets.ModelViewSet):
     queryset = Chart.objects.all()
     serializer_class = ChartSerializer
+
+
+def home(response):
+    return render(response, 'ChartJS/base.html', {})
+
+
+def index(response):
+    return render(response, 'ChartJS/home.html', {})
+
+
+def get_data(request):
+    server = 'vwpnwrsiwhsql.prodwr.vwpn.emea.vwg'
+    database = 'IWH'
+    username = 'M1_Qlik'
+    password = 'M1_Qlik'
+    connection = pyodbc.connect(
+        'DRIVER={SQL SERVER};SERVER=' + server + ';DATABASE=' + database + ';ENCRYPT=false;UID=' + username + ';PWD=' + password)
+
+    request = pd.read_sql("""   SELECT TOP 10 data, hour_number, ist
+                                FROM IWH.dbo.M1_Counters_History
+                                WHERE shift_number <>0 and hour_number<>0  and linia='FA7';""", connection)
+    data = request.to_json()
+
+
+    # data = {
+    #     'sales': 100,
+    #     'customers': 10,
+    # }
+    return JsonResponse(data, safe=False)
