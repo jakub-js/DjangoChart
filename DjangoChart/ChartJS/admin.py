5fcdced2350c8c95b56461573cc1d4fb072c1c29
jakub-js <szczepanski.jakub06@gmail.com>
2022-10-27 09:55:45 +0200

    init3

--- DjangoChart/ChartJS/admin.py
+++ DjangoChart/ChartJS/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Chart
 
+
 # Register your models here.
-admin.site.register(Chart)
+# admin.site.register(Chart)
+
+@admin.register(Chart)  # dokorowanie
+class ChartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description']
+    list_filter = ['title']
+    search_fields = ['title']
